f40734a9f7818cbc7df39b1aef6a197417fdf984
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-01-11 09:06:57 +0000

    ApplicationControllerに記述する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # devise利用の機能が使われる前にconfigure_permitted_parametersメソッドが実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
