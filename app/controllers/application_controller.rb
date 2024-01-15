e3e1cd1bb3b2b7f835a9a74eeaa7b48135ce6f39
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-01-15 06:14:16 +0000

    applicationコントローラに追記する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # devise利用の機能が使われる前にconfigure_permitted_parametersメソッドが実行される
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後にabout画面に遷移する
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
   protected
   # configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッド
   # を使うことでユーザー登録の際に、nameのデータ操作を許可する
