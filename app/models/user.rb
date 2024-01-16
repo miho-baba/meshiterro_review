210b1cf8f9f75d983d25d5c82619d8dc1e94b16a
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-01-16 13:26:29 +0000

    UserモデルにPostImageモデルを関連つける

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  # 下記の記述は、たくさんpost_imageを持っているという意味である
   has_many :post_images, dependent: :destroy
 
 end
