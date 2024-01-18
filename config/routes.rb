a1fd7100700424264bbbc026b424aa137940bade
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-01-18 07:45:49 +0000

    ルーティングの記述を変更する

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
+  root to: "homes#top"
   devise_for :users
+  resources :post_images, only: [:new, :index, :show]
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     get "homes/about" => "homes#about"
 end
