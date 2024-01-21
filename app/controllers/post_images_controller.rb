838501369582bbf441fb492041a554f5570cf705
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-01-21 15:04:37 +0000

    コントローラ：登録機能追記する

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,12 +3,21 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
+  # 投稿データの保存
   def create
+    
   end
 
   def index
   end
 
   def show
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+  
+  def post_image_params
+    
   end
 end
