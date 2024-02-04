099c74ad21d69e19dd16c067d1170bdd029c5a83
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-02-04 12:58:05 +0000

    userモデルに追記する

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
   # 下記の記述は、たくさんpost_imageを持っているという意味である
   has_many :post_images, dependent: :destroy
 
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      
 end
