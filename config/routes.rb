1beb31fd00a519f980ffd753a3caf7379f9217d6
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-02-05 09:23:46 +0000

    usersコントローラを作成する

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
   devise_for :users
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    get 'homes/about' => 'homes#about'
 end
