bbe95817cd931c47dd51e3d1e6d77a9d9180fbb5
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-02-07 08:18:32 +0000

    マイページリンクを記述する

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit]
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    delete '/users/sign_out' => 'devise/sessions#destroy'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
