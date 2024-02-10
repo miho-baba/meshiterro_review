86862b4c8ca66819965d166ed070021fa75cc8ca
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-02-10 13:18:19 +0000

    userの更新機能を追記する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = 
+    @user.
+    redirect_to 
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
