ece32a9af1dc236811d652a0501d6a8ba52a59fc
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-02-22 15:00:30 +0000

    ルーティングを追記する

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create]
+    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
   devise_scope :user do
