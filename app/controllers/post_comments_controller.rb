3f31d82a94ccd87e6ab3db326748993bcc363706
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2024-02-25 14:36:38 +0000

    postcommentsコントローラにdestroyアクションを追記する

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -10,7 +10,7 @@ class PostCommentsController < ApplicationController
   
   def destroy
     PostComment.find(params[:id]).destroy
-    
+    redirect_to post_image_path(params[:post_image_id])
   end
 
   private
